9f9331d6fa4cd4bb42e1917fc86d810b8038e227
Francisco Garrido-Valenzuela <f.garridov@uc.cl>
2023-06-09 15:21:08 +0200

    Add files via upload

---
+++ applied_sam.py
@@ -0,0 +1,144 @@
+import os
+
+import groundingdino.datasets.transforms as T
+import numpy as np
+import torch
+from groundingdino.models import build_model
+from groundingdino.util import box_ops
+from groundingdino.util.inference import predict
+from groundingdino.util.slconfig import SLConfig
+from groundingdino.util.utils import clean_state_dict
+from huggingface_hub import hf_hub_download
+from segment_anything import sam_model_registry
+from segment_anything import SamPredictor
+from matplotlib import pyplot as plt
+import warnings
+from PIL import Image
+
+SAM_MODELS = {
+    "vit_h": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth",
+    "vit_l": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth",
+    "vit_b": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth"
+}
+
+CACHE_PATH = os.environ.get("TORCH_HOME", os.path.expanduser("~/.cache/torch/hub/checkpoints"))
+
+
+def load_model_hf(repo_id, filename, ckpt_config_filename, device='cpu'):
+    cache_config_file = hf_hub_download(repo_id=repo_id, filename=ckpt_config_filename)
+
+    args = SLConfig.fromfile(cache_config_file)
+    model = build_model(args)
+    args.device = device
+
+    cache_file = hf_hub_download(repo_id=repo_id, filename=filename)
+    checkpoint = torch.load(cache_file, map_location='cpu')
+    log = model.load_state_dict(clean_state_dict(checkpoint['model']), strict=False)
+    print(f"Model loaded from {cache_file} \n => {log}")
+    model.eval()
+    return model
+
+
+def transform_image(image) -> torch.Tensor:
+    transform = T.Compose([
+        T.RandomResize([800], max_size=1333),
+        T.ToTensor(),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ])
+
+    image_transformed, _ = transform(image, None)
+    return image_transformed
+
+
+class LangSAM():
+
+    def __init__(self, sam_type="vit_h"):
+        self.sam_type = sam_type
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.build_groundingdino()
+        self.build_sam(sam_type)
+
+    def build_sam(self, sam_type):
+        checkpoint_url = SAM_MODELS[sam_type]
+        try:
+            sam = sam_model_registry[sam_type]()
+            state_dict = torch.hub.load_state_dict_from_url(checkpoint_url)
+            sam.load_state_dict(state_dict, strict=True)
+        except:
+            raise ValueError(f"Problem loading SAM please make sure you have the right model type: {sam_type} \
+                and a working checkpoint: {checkpoint_url}. Recommend deleting the checkpoint and \
+                re-downloading it.")
+        sam.to(device=self.device)
+        self.sam = SamPredictor(sam)
+
+    def build_groundingdino(self):
+        ckpt_repo_id = "ShilongLiu/GroundingDINO"
+        ckpt_filename = "groundingdino_swinb_cogcoor.pth"
+        ckpt_config_filename = "GroundingDINO_SwinB.cfg.py"
+        self.groundingdino = load_model_hf(ckpt_repo_id, ckpt_filename, ckpt_config_filename)
+
+    def predict_dino(self, image_pil, text_prompt, box_threshold, text_threshold):
+        image_trans = transform_image(image_pil)
+        boxes, logits, phrases = predict(model=self.groundingdino,
+                                         image=image_trans,
+                                         caption=text_prompt,
+                                         box_threshold=box_threshold,
+                                         text_threshold=text_threshold,
+                                         device=self.device)
+        W, H = image_pil.size
+        boxes = box_ops.box_cxcywh_to_xyxy(boxes) * torch.Tensor([W, H, W, H])
+
+        return boxes, logits, phrases
+
+    def predict_sam(self, image_pil, boxes):
+        image_array = np.asarray(image_pil)
+        self.sam.set_image(image_array)
+        transformed_boxes = self.sam.transform.apply_boxes_torch(boxes, image_array.shape[:2])
+        masks, _, _ = self.sam.predict_torch(
+            point_coords=None,
+            point_labels=None,
+            boxes=transformed_boxes.to(self.sam.device),
+            multimask_output=False,
+        )
+        return masks.cpu()
+
+    def predict(self, image_pil, text_prompt, box_threshold=0.3, text_threshold=0.25):
+        boxes, logits, phrases = self.predict_dino(image_pil, text_prompt, box_threshold, text_threshold)
+        masks = torch.tensor([])
+        if len(boxes) > 0:
+            masks = self.predict_sam(image_pil, boxes)
+            #masks = masks.squeeze(1)
+        return masks, boxes, phrases, logits
+
+def prompt_sam(prompt: str, image_path: str, threshold: float = None, warns: bool = True):
+    if warns == False:
+        warnings.filterwarnings("ignore")
+    model = LangSAM()
+    image_pil = Image.open(image_path).convert("RGB")
+    masks, boxes, phrases, logits = model.predict(image_pil, prompt)
+    # removing elements from torch tensors
+    if threshold is not None:
+        for i, l in enumerate(logits):
+            if l < threshold:
+                masks = np.delete(masks, i, axis=0)
+                boxes = np.delete(boxes, i, axis=0)
+                phrases = np.delete(phrases, i, axis=0)
+                logits = np.delete(logits, i, axis=0)
+    return masks, boxes, phrases, logits
+
+def show_mask(mask, ax):
+    color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+    
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
+    
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))  
